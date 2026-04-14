032bf63b293571f8622a888df43cc7346005822e
Nikro07 <adriancarloso077@gmail.com>
2026-04-15 07:12:26 +0800

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,39 @@
+window.songs = [
+    {
+        title: "Valentine",
+        artist: "Laufey",
+        src: "music/music4.mp3",
+        image: "musicIcons/Valentine(CV).jfif"
+    },
+    {
+        title: "Snow",
+        artist: "Ricky Monte",
+        src: "music/music1.mp3",
+        image: "musicIcons/Snow.jfif"
+    },
+    {
+        title: "Good Lookin'",
+        artist: "Dixon Daryl",
+        src: "music/music3.mp3",
+        image: "musicIcons/Good_Looking(CV).png"
+    },   
+    {
+        title: "Limasawa Street (Steve Lillywhite Mix)",
+        artist: "Ben&Ben",
+        src: "music/music5.mp3",
+        image: "musicIcons/lima.png"
+    },      
+    {
+        title: "Kubli",
+        artist: "3AM",
+        src: "music/music6.mp3",
+        image: "musicIcons/kubli.jfif"
+    },      
+    {
+        title: "Your Universe",
+        artist: "Rico Blanco",
+        src: "music/music2.mp3",
+        image: "musicIcons/YU.png"
+    }
+];
+
